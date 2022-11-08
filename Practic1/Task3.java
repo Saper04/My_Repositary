9314672b89558233e9701c838b25f72c5fe64e06
Saper04 <vasilenko.dan04@mail.ru>
2022-11-08 15:00:01 +0300

    Update Task3.java

--- Practic1/Task3.java
+++ Practic1/Task3.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         int[] a = new int[6];
         int sum = 0;
-        int srAR = 0;
+        double srAR = 0;
         for (int i = 1; i<=5; i++) {
             a[i] = i;
             sum = sum+a[i];
